96189ecf5a4e0e3a25aefc18af11cc80761a8176
Çiğdem Türkmen <cigdemturkmen57@outlook.com>
2021-12-01 13:42:38 +0300

    Add project files.

---
+++ CoreWebAPI/Controllers/CategoryController.cs
@@ -0,0 +1,66 @@
+﻿using CoreWebAPI.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreWebAPI.Controllers
+{
+    [Route("api/[controller]/[action]")] // www.google.com/api/category/getcategories ..route yapısını nasıl istiyosak öyle yazabiliriz.
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        /* http status codes
+         * 1 information
+         * 2 success
+         * 3 redirect
+         * 4 client error
+         * 5 server error
+         * */
+
+        [HttpGet]
+        public IActionResult Get() // ../api/category/get
+        {
+            var category = "içecekler";
+            return Ok(category);  // farklı dönüş tipleri var. view yok. Ok() 200 başarılı işlem
+        }
+
+        [HttpPost]
+        public IActionResult Add(CategoryDTO model) //dto'yu viewmodel olarak düşünebilirsin
+        {
+            // logic
+            return Ok("eklendi");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id) // .../api/category/GetById/3
+        {
+            if (id == 0)
+            {
+                return ValidationProblem("Category 0 veya null olamaz"); //BadRequest de return edebilirsin.
+            }
+
+            // db'den veriyi çek client'a gönder
+
+            return Ok(new CategoryDTO() { CategoryName = "içecekler", Description = "su kola fanta" });
+        }
+
+        [HttpPut]
+        public IActionResult Update(CategoryDTO dTO)
+        {
+            // logic
+            return Ok("Düzenleme yapıldı."); // ok= servisten sonuç döndü demek.
+        }
+
+        [HttpDelete("{id}")] // web api'deki routing yapısından dolayı bunları belirtiyoruz.
+        public IActionResult Delete(int id)
+        {
+            // logic
+            return Ok("silindi");
+        }
+
+
+    }
+}
---
+++ CoreWebAPI/Startup.cs
@@ -0,0 +1,59 @@
+using Blog.Data;
+using Blog.Data.Entities;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreWebAPI
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<BlogDbContext>(option =>
+            {
+                option.UseSqlServer("server=.")
+            });
+
+            services.AddScoped<IRepository<Category>, EFRepository<Category>>();
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            var devName = Configuration["developer"];
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
